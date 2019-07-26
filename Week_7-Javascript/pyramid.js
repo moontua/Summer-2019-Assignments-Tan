5fd730173b7536d8aebed1c33b7d2dcdcfa46e34
Ken <kenlai@Kens-MacBook.local>
2019-07-26 11:07:25 -0700

    7th weeks + challenge

--- Week_7-Javascript/pyramid.js
+++ Week_7-Javascript/pyramid.js
@@ -1,19 +1,13 @@
-for (starArray = 1; starArray <= 5; starArray++) {
-    if (starArray <= 1) {
-        console.log("*");
-        if (starArray <= 2) {
-            console.log("* *");
-            if (starArray <= 3) {
-                console.log("* * *");
-                if (starArray <= 4) {
-                console.log("* *");
-                if (starArray <= 5) {
-                console.log("*");
-            }
-            }
-            }
-        }
-
+var starArray = "*";
+for (var starArray = 1, starArray <= 5; starArray++) {
+    if (starArray == 1 && starArray == 5) {
+        starArray1 = starArray;
     }
-
+    if (starArray == 2 && starArray == 4) {
+        starArray2 = starArray + 1;
+    }
+    if (starArray == 3) {
+        starArray3 = starArray + 3;
+    }
 }
+console.log(starArray);
