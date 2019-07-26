1e7adc264a1eaf7e882b3a57e51edad49d25ae7e
Ken <kenlai@Kens-MacBook.local>
2019-07-26 10:20:49 -0700

    7th weeks now

---
+++ Week_7-Javascript/pyramid.js
@@ -0,0 +1,19 @@
+for (starArray = 1; starArray <= 5; starArray++) {
+    if (starArray <= 1) {
+        console.log("*");
+        if (starArray <= 2) {
+            console.log("* *");
+            if (starArray <= 3) {
+                console.log("* * *");
+                if (starArray <= 4) {
+                console.log("* *");
+                if (starArray <= 5) {
+                console.log("*");
+            }
+            }
+            }
+        }
+
+    }
+
+}
